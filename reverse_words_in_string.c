77f17702f3a6b39fcc20eb980995a918d5190f19
Nouran Hisham <hishamnouran4@gmail.com>
2022-07-09 21:00:13 +0200

    Create reverse_words_in_string.c

---
+++ reverse_words_in_string.c
@@ -0,0 +1,31 @@
+
+void reverse(char* begin, char* end)
+{
+    char temp;
+    while (begin < end) {
+        temp = *begin;
+        *begin++ = *end;
+        *end-- = temp;
+    }
+}
+
+
+char * reverseWords(char * s){
+    
+    char* word_begin = s;
+    char* temp = s;
+    
+    while (*temp) {
+        temp++;
+        if (*temp == '\0') {
+            reverse(word_begin, temp - 1);
+        }
+        else if (*temp == ' ') {
+            reverse(word_begin, temp - 1);
+            word_begin = temp + 1;
+        }
+    }
+    
+    return s;
+
+}
